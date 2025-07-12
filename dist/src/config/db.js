052728a4cbe3876340629a4d05510ebdade79d29
Ferdous Sohag <hassanferdous101@gmail.com>
2025-07-12 20:19:07 +0600

    remove dist

--- dist/src/config/db.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.db = void 0;
-require("dotenv/config");
-const node_postgres_1 = require("drizzle-orm/node-postgres");
-const _1 = require(".");
-exports.db = (0, node_postgres_1.drizzle)(_1.config.db.uri);
